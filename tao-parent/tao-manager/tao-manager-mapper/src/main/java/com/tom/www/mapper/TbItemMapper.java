8005bc07d3b1a2d300b4a864315e670ce8aa0104
IDLC1 <licyafighting@gmail.com>
2020-11-25 21:28:34 +0800

    11

---
+++ tao-parent/tao-manager/tao-manager-mapper/src/main/java/com/tom/www/mapper/TbItemMapper.java
@@ -0,0 +1,7 @@
+package com.tom.www.mapper;
+
+import com.taotao.pojo.TbItem;
+
+public interface TbItemMapper {
+    TbItem selectByPrimaryKey(long id);
+}
---
+++ tao-parent/tao-manager/tao-manager-pojo/src/main/java/com/tom/www/pojo/TbItemDesc.java
@@ -0,0 +1,47 @@
+package com.tom.www;
+
+
+public class TbItemDesc {
+
+  private long itemId;
+  private String itemDesc;
+  private java.sql.Timestamp created;
+  private java.sql.Timestamp updated;
+
+
+  public long getItemId() {
+    return itemId;
+  }
+
+  public void setItemId(long itemId) {
+    this.itemId = itemId;
+  }
+
+
+  public String getItemDesc() {
+    return itemDesc;
+  }
+
+  public void setItemDesc(String itemDesc) {
+    this.itemDesc = itemDesc;
+  }
+
+
+  public java.sql.Timestamp getCreated() {
+    return created;
+  }
+
+  public void setCreated(java.sql.Timestamp created) {
+    this.created = created;
+  }
+
+
+  public java.sql.Timestamp getUpdated() {
+    return updated;
+  }
+
+  public void setUpdated(java.sql.Timestamp updated) {
+    this.updated = updated;
+  }
+
+}
